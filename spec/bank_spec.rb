21bb5f7a1432b4c4b12e9244ac4f60bd6b532749
gennyallcroft <gennyallcroft@hotmail.co.uk>
2019-08-27 13:56:37 +0100

    balance has default of 0

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,8 +2,9 @@ require 'bank'
 
 describe Bank do
 
-  describe '#account_statement' do
-
-
+  describe '#balance' do
+    it 'has default balance of 0' do
+      expect(subject.balance).to eq 0
+    end
   end
 end
